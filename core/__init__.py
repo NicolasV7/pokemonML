1a705cc9c99ea3a4f3af886a028aa2c9ce6b38e6
Nicolas <nramirezv1@correo.usbcali.edu.co>
2025-04-11 11:35:07 -0500

    Enhance Flask app initialization by adding static folder configuration

--- core/__init__.py
+++ core/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 import os
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates')
+    app = Flask(__name__,
+                static_folder='../static',
+                template_folder='../templates')
     app.config.from_mapping(
         MODEL_PATH=os.path.join(app.root_path, '../models/kmeans_model.pkl'),
         SCALER_PATH=os.path.join(app.root_path, '../models/scaler.pkl'),
